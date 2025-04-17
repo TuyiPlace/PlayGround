c295324242685819d706a02571e909e9cb140324
TuyiPlace <sr0peste@gmail.com>
2025-04-17 19:43:11 -0400

    script contador

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log('Prueba1')
+let contador = 0
+function sumador() {
+    contador++
+    document.querySelector(".pantalla1").textContent = contador;
+}
+document.querySelector(".Click1").addEventListener("click", sumador);
