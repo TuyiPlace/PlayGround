cec4bd168c52db46a1b5bdc9145af308b149d87a
TuyiPlace <sr0peste@gmail.com>
2025-05-07 19:35:20 -0400

    se agregan 4 botones sin funcionalidad

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
+function wait(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 let contador = 0
 function sumador() {
     contador++
     document.querySelector(".pantalla1").textContent = contador;
 }
 document.querySelector(".Click1").addEventListener("click", sumador);
+
+let botonesApretados = document.querySelectorAll(".BotonColor");
+botonesApretados.forEach(boton =>{
+    console.log(boton)
+})
